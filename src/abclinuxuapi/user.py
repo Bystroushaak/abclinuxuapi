7444c001258d5f5a0094bdfba49fab1d664d46a6
Bystroushaak <bystrousak@kitakitsune.org>
2015-05-30 18:29:26 +0200

    #6: Added .__repr__() for User.

--- src/abclinuxuapi/user.py
+++ src/abclinuxuapi/user.py
@@ -387,3 +387,6 @@ class User(object):
     def __iter__(self):
         for blog in self.get_blogposts():
             yield blog
+
+    def __repr__(self):
+        return "%s(%s)" % (self.__class__.__name__, self.username)
